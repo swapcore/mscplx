23e0a14433566a9f7e51e702c29e0a49647d9edb
Oscar Elías <mepple_505@hotmail.com>
2024-10-29 20:51:15 -0600

    Inicializar libreria: title.c

---
+++ tools/title.h
@@ -0,0 +1,27 @@
+/*
+	Tratamiento del titulo de la canción
+	Copyright (c) 2024 Oscar Elías
+	
+	This file is part of mscplx.
+	
+	mscplx is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published
+	by the Free Software Foundation, either version 3 of the License,
+	or (at your option) any later version.
+	
+	mscplx is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of 
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with mscplx. If not, see 
+	<https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _TITLE_H_
+#define _TITLE_H_
+
+
+
+#endif
